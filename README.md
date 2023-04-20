106f54e0cc08e97dea67fa39c1aae19b3b9c621b
Dawn Kline <dawn.kline.m@gmail.com>
2023-04-19 21:19:51 -0500

    added localStorage functionality

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ This is Challenge #6, this project builds a weather dashboard runs in the browse
 
  Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. 
 
-<!-- Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. The base URL should look like the following: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate. -->
+ The [5 Day Weather Forecast](https://openweathermap.org/forecast5) is utilized to retrieve weather data for cities. The base URL should look like the following: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`. 
+ 
+ After registering for a new API key, the API key will activate.
 
-<!-- **Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name? -->
+`localStorage` is used to store persistent data. 
 
-<!-- You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys). -->
+<!-- For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys). --> -->
 
 ## User Story
 
@@ -33,9 +35,9 @@ When the user clicks on a city in the search history, the user is presented with
 
 ## Mock-Up
 
-The following image shows the web application's appearance and functionality:
+<!-- The following image shows the web application's appearance and functionality:
 
-<!-- ![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png) -->
+./Assets/06-server-side-apis-homework-demo.png -->
 
 ## Location
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 
 let locateCity = "London"
+let searchHistory = JSON.parse(localStorage.getItem('forecast')) || []
 
 // event listener
 function getApi(locateCity) {
@@ -15,6 +16,11 @@ function getApi(locateCity) {
             // let firstCity = citiesFound[0];
             let latEl = cityData.coord.lat;
             let lonEl = cityData.coord.lon;
+            searchHistory.push(cityData.name)
+            localStorage.setItem("forecast", JSON.stringify(searchHistory))
+
+            //add button for city
+
             // console.log(firstCity)
             // console.log(firstCity.lat);
             // console.log(firstCity.lon);
@@ -25,15 +31,37 @@ function getApi(locateCity) {
             document.querySelector("#cityWind").textContent = `Wind: ${cityData.wind.speed} MPH`
             // this fetch request is based on prior query
             // this is the 5-day weather forecast that acquires the lat/long data and gets the forecast
-            return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latEl}&lon=${lonEl}&appid=4d1c0a79f5410788bbe883c406a19675`)
+            return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latEl}&lon=${lonEl}&appid=4d1c0a79f5410788bbe883c406a19675&units=imperial`)
 
         })
 
         .then(response => response.json())
         .then(forecastData => {
 
-            console.log(forecastData);
+            console.log(forecastData.list);
             //add forecast cards here
+            for (let i = 0; i < forecastData.list.length; i++) {
+                const element = forecastData.list[i];
+                if (element.dt_txt.includes("12:00:00")) {
+                    let column = document.createElement("div")
+                    column.setAttribute("class", "col")
+                    let card = document.createElement("div")
+                    card.setAttribute("class", "card")
+                    let cardbody = document.createElement("div")
+                    cardbody.setAttribute("class", "card-body")
+                    let cardtitle = document.createElement("h5")
+                    cardtitle.setAttribute("class", "card-title")
+                    cardtitle.textContent = element.dt_txt.split(" ")[0]
+                    let cardtext = document.createElement("p")
+                    cardtext.setAttribute("class", "card-text")
+                    cardtext.textContent = element.main.temp
+
+                    cardbody.append(cardtitle, cardtext)
+                    card.append(cardbody)
+                    column.append(card)
+                    document.querySelector(".forecast").append(column)
+                }
+            }
         })
 }
 let fetchButton = document.querySelector("#search")
